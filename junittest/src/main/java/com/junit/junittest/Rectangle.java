b7b2cc75d88319211e8a21a367b31c2c90fab961
Yash <yash@gmail.com>
2022-04-17 17:49:21 +0530

    Making project

---
+++ junittest/src/main/java/com/junit/junittest/Rectangle.java
@@ -0,0 +1,14 @@
+package com.junit.junittest;
+
+public class Rectangle {
+    
+    private final double width,height;
+
+    public Rectangle(){
+        this.width=1;
+        this.height=1;
+    }
+
+    
+
+}
---
+++ junittest/src/test/java/com/junit/junittest/JunittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junit.junittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
